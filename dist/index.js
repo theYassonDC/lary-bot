6f2862367d29dfcd90b504e84c7f2f89d9f8c641
YassonDC <yassonword@gmail.com>
2024-12-08 18:14:12 -0500

    modify packages

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const seyfert_1 = require("seyfert");
 const cooldown_1 = require("@slipher/cooldown");
-const express_1 = __importDefault(require("express"));
 const client = new seyfert_1.Client({
     commands: {
         prefix: (_msg) => {
@@ -19,12 +15,4 @@ const client = new seyfert_1.Client({
 client.start().then(() => {
     client.cooldown = new cooldown_1.CooldownManager(client);
     client.loadEvents();
-});
-const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
-app.get('/', (_req, res) => {
-    res.send('Hello World!');
-});
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
 });
