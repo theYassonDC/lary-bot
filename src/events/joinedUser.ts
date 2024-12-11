dabf21cb5d97ad5c6e2347848be86372932709f9
YassonDC <yassonword@gmail.com>
2024-12-11 03:48:50 -0500

    Add event joined

--- src/events/joinedUser.ts
+++ src/events/joinedUser.ts
@@ -4,6 +4,7 @@ import { config, joinedReply } from "../utils";
 export default createEvent({
   data: { once: false, name: "guildMemberAdd" },
   async run(user, client) {
+    if (user.user.bot) return
     const channel = await client.channels.fetch(config.channel)
     if (channel.isTextGuild()) {
       const random = Math.floor(Math.random() * joinedReply.length)
